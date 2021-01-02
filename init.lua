c50598cb43976a35a3d35477981f3a8e0056904f
Seb Renauld <seb.renauld@gmail.com>
2021-01-02 20:52:00 +0100

    Set meta table so unknown i18n keys do not return nil

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
 local AddonName, MDT = ...
 _G["DungeonTools"] = MDT
+local _L = {}
 MDT.L = {}
+local i18nMetaTable = {
+    __index = function(t, k)
+        if _L[k] == nil then
+            return k
+        else
+            return _L[k]
+        end
+    end,
+    __newindex = function(t, k, v)
+        _L[k] = v
+    end
+}
+setmetatable(MDT.L, i18nMetaTable)
