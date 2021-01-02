a38484e8fdcbf7b416e54e5d357fe8e621398226
CalistusK <CalistusK@users.noreply.github.com>
2021-01-02 14:46:48 -0800

    fix: update affix week dropdown on map change
    Resolves #79 when following the repro steps as described in the issue, as well as when using Alt+Scroll.

--- DungeonTools.lua
+++ DungeonTools.lua
@@ -2767,6 +2767,7 @@ function MDT:CreateDungeonSelectDropdown(frame)
         else
             MDT:UpdateToDungeon(key)
         end
+        frame.sidePanel.affixDropdown:SetAffixWeek(MDT:GetCurrentPreset().week or MDT:GetCurrentAffixWeek() or 1)
 	end)
 	group:AddChild(group.DungeonDropdown)
 
